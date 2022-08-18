1b1638f924feace998c0ae06ac6efbb4d1027544
vilte4 <34207060+vilte4@users.noreply.github.com>
2022-08-19 00:06:32 +0300

    Add company card component with styling

---
+++ src/Components/CompanyCard.js
@@ -0,0 +1,54 @@
+import { LinkItUrl } from "react-linkify-it";
+import {
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@material-ui/core";
+import classes from "./CompanyCard.module.css";
+import buttonClasses from "./Button.module.css";
+
+const CompanyCard = (props) => {
+  const cardHeaderClickHandler = () => {
+    console.log("labas");
+  };
+
+  return (
+    <div>
+      <Card
+        style={{
+          border: "2px solid purple",
+          boxShadow: "5px 10px #ece6ff",
+        }}
+        className={classes.root}
+      >
+        <CardHeader
+          title={props.companyInfo.name}
+          className={buttonClasses.button}
+          style={{ borderRadius: "0px" }}
+          onClick={cardHeaderClickHandler}
+        />
+        <CardMedia className={classes.media}>
+          <img
+            className={classes.media}
+            alt="product"
+            src={props.companyInfo.logo}
+          />
+        </CardMedia>
+
+        <CardContent>
+          <Typography noWrap={true}>
+            Country: {props.companyInfo.country}
+          </Typography>
+          <Typography noWrap={true}>
+            Currency: {props.companyInfo.currency}
+          </Typography>
+          <LinkItUrl>{props.companyInfo.weburl}</LinkItUrl>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CompanyCard;
