982dfe5aedd9142372653feb5bdabedad1a0ecf2
vilte4 <34207060+vilte4@users.noreply.github.com>
2022-08-19 00:07:22 +0300

    Add candlestick chart for stock graph

---
+++ src/Components/CandlestickChart.js
@@ -0,0 +1,38 @@
+import ReactApexChart from "react-apexcharts";
+
+const CandlestickChart = (props) => {
+  const chartConfig = {
+    options: {
+      chart: {
+        type: "candlestick",
+        height: 350,
+      },
+      title: {
+        text: "CandleStick Chart",
+        align: "left",
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        tooltip: {
+          enabled: true,
+        },
+      },
+    },
+  };
+
+  return (
+    <div id="chart">
+      <ReactApexChart
+        options={chartConfig.options}
+        series={props.chartData}
+        type="candlestick"
+        height={350}
+      />
+      // {console.log(chartConfig.series)}
+    </div>
+  );
+};
+
+export default CandlestickChart;
